8821743a1dd0b6179743193f381af9cac29af9e3
Inv1ctus <137711880+R-Abinav@users.noreply.github.com>
2025-07-18 18:42:10 +0530

    chore: some minor changes in pointers

--- cmd/pointers/main.go
+++ cmd/pointers/main.go
@@ -6,37 +6,39 @@ import (
 
 func main(){
 	var myPtr *int32 
-	fmt.Printf("\n The value of the ptr is : %v", myPtr)
+	fmt.Printf("\nThe value of the ptr is : %v", myPtr)
 
 	var myPtr2 *int32 = new(int32)
-	fmt.Printf("\n The value of the ptr is : %v", myPtr2)
+	fmt.Printf("\nThe value of the ptr is : %v", myPtr2)
 
 	var i int32 = 0
-	fmt.Printf("\n The value of i is: %v", i)
+	fmt.Printf("\nThe value of i is: %v", i)
 
 	var ptrToi *int32 = &i
 	*ptrToi = 10
 
-	fmt.Printf("\n The value of the ptr to i: %v", ptrToi)
-	fmt.Printf("\n The value of i: %v", i)
-	fmt.Printf("\n The defereferenced value of ptr to i: %v", *ptrToi)
+	fmt.Printf("\nThe value of the ptr to i: %v", ptrToi)
+	fmt.Printf("\nThe value of i: %v", i)
+	fmt.Printf("\nThe defereferenced value of ptr to i: %v", *ptrToi)
 
 	// -- NOTE --
 	//slices contain pointers to an underlying array
 	var mySlice []int32 = []int32{1,2,3}
+	fmt.Printf("\nThe slice before any operations: %v", mySlice)
+
 	var mySliceCopy = mySlice
 
 	mySliceCopy[1] = 10
 
 	fmt.Println("\nOriginal slice: ", mySlice)
-	fmt.Println("\nCopied Slice:", mySliceCopy)
+	fmt.Println("Copied Slice:", mySliceCopy)
 
 	// -- Pass by reference --
 	// You wanna pass by reference if your handling large data -> As pass by value creates copies
 	var thing1 [5]int32 = [5]int32{1,2,3,4,5}
 	var result [5]int32 = squareThing(&thing1)
 
-	fmt.Printf("\nThe original thing: %v", thing1)
+	fmt.Printf("The original thing: %v", thing1)
 	fmt.Printf("\nThe result is: %v", result)
 }
 
