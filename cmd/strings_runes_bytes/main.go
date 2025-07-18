f03d36d0b56912868fabc508763f626e005a8361
Inv1ctus <137711880+R-Abinav@users.noreply.github.com>
2025-07-18 16:43:18 +0530

    chore: strings, runes, bytes

---
+++ cmd/strings_runes_bytes/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main(){
+	//é -> Non ascii character
+	var myString = "résumé" 
+	var indexed = myString[1]
+
+	fmt.Printf("%v, %T\n", indexed, indexed)
+
+	for i,v := range myString{
+		fmt.Println(i,v)
+	}
+
+	fmt.Printf("\nThe length of my string is %v", len(myString))
+
+	// --- Runes ---
+	var myRune = []rune("résumé")
+	fmt.Printf("\nThe length of my string (rune) is %v\n", len(myRune))
+
+	for i,v := range myRune{
+		fmt.Println(i,v)
+	}
+
+	//NOTE: You can declare a run type like this as well
+	var anotherRune = 'a'
+	fmt.Println(anotherRune)
+
+	//NOTE: Strings are immutable
+	//String Concatenation (2 ways -> Inefficient and efficient)
+	//1. Inefficient (Because it makes a new string every time you concatenate)
+	var strSlice = []string{"h","e", "l", "l", "o","g","o"}
+	var catStr = ""
+
+	for i := range strSlice{
+		catStr += strSlice[i]
+	}
+
+	fmt.Printf("\n%v", catStr)
+
+	//2. Using string builder -> Better
+	var strBuilder strings.Builder
+	for i := range strSlice{
+		strBuilder.WriteString(strSlice[i])
+	}
+
+	var catStr2 = strBuilder.String()
+	fmt.Println(catStr2)
+	
+}
