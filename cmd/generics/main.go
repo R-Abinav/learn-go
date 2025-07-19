a0f1cb31d67974a378a20247fac9050b56dc5d19
Inv1ctus <137711880+R-Abinav@users.noreply.github.com>
2025-07-19 15:50:01 +0530

    chore: generics, more examples

--- cmd/generics/main.go
+++ cmd/generics/main.go
@@ -10,6 +10,12 @@ func main(){
 
 	var floatSlice []float32 = []float32{2,3,4}
 	fmt.Println(sumSlice[float32](floatSlice))
+
+	var nonEmptySlice []string = []string{"hello", "bigman"}
+	isEmpty[string](nonEmptySlice)
+
+	var emptySlice []int = []int{}
+	isEmpty[int](emptySlice)
 }
 
 func sumSlice[T int | float32 | float64](slice []T) T{
@@ -18,4 +24,12 @@ func sumSlice[T int | float32 | float64](slice []T) T{
 		sum += v
 	}
 	return sum
+}
+
+func isEmpty[T any](slice []T){
+	if len(slice) == 0{
+		fmt.Println("True, slice is empty")
+	}else{
+		fmt.Println("False, slice is not empty")
+	}
 }
