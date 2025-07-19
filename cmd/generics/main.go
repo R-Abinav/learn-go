ba34511aaae4d404acbe996605f15bda6541ee9f
Inv1ctus <137711880+R-Abinav@users.noreply.github.com>
2025-07-19 15:47:22 +0530

    chore: generics

--- cmd/generics/main.go
+++ cmd/generics/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main(){
-	
+	var intSlice []int = []int{1,2,3,4,5}
+	fmt.Println(sumSlice[int](intSlice))
+
+	var floatSlice []float32 = []float32{2,3,4}
+	fmt.Println(sumSlice[float32](floatSlice))
+}
+
+func sumSlice[T int | float32 | float64](slice []T) T{
+	var sum T
+	for _, v := range slice{
+		sum += v
+	}
+	return sum
 }
